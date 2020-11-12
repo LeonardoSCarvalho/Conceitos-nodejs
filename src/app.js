0e56799f74cee70f3c4ba9043ddc8d7d4bec994c
Leonardo Carvalho <leo.carvalholkp@gmail.com>
2020-11-12 16:08:08 -0300

    Projects CRUD

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+ const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,48 @@ app.use(cors());
 
 const repositories = [];
 
+function validateId(request, response, next){
+  const { id } = request.params;
+  if(!isUuid(id)){
+    return response.status(400).json({ error: 'Invalid id'});
+  }
+  return next();
+}
+
+app.use('/repositories/:id', validateId);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = {id: uuid(), title, url, techs, likes: 0};
+  repositories.push(repository);
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+  const index = repositories.findIndex(repositories => repositories.id === id);
+  repositories[index] = {id, title, url, techs, likes: repositories[index].likes};
+  return response.json(repositories[index]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex(repositories => repositories.id === id);
+  repositories.splice(index, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repository = repositories.find(repositories => repositories.id === id);
+  repository.likes++;
+  return response.json(repository);
 });
 
 module.exports = app;
